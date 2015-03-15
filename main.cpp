b812d50899c41a7468f4726de75998765f58d2be
confiante <eddiemambo@gmail.com>
2015-03-14 23:53:47 -0400

    new main.cpp

---
+++ main.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+
+#include "FOODS.h"
+
+
+
+using namespace std;
+
+
+
+
+
+int main()
+
+{
+
+
+
+    string name;
+
+    double calories;
+
+    double servings;
+
+    
+
+    
+
+    cout << "What is your name? \n";
+
+    cin >> name;
+
+    
+
+    cout << "How many calories do you have left to eat? \n";
+
+    cin >> calories;
+
+    
+
+    cout << "Here's what you can eat: \n";
+
+    
+
+    return 0;
+
+
+
+}
